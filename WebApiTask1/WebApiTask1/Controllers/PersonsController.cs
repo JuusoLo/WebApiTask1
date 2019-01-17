8dca8760a3df38ed03459f48ec5ebfd092a92700
Juuso Lonka <1701537@student.saimia.fi>
2019-01-17 11:12:33 +0200

    WebApiTask1 Update

---
+++ WebApiTask1/WebApiTask1/Controllers/PersonsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiTask1.Models;
+using WebApiTask1.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiTask1.Controllers
+{
+    [Route("api/[controller]")]
+    public class PersonsController : Controller
+    {
+        private readonly IPersonRepository _personRepository;
+
+        public PersonsController(IPersonRepository personRepository)
+        {
+            _personRepository = personRepository;
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult <List<Person>> Get()
+        {
+            return _personRepository.Read();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public ActionResult <List<Person>> Get(int id)
+        {
+            var persons = _personRepository.Read(id);
+            return new JsonResult(persons);
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public ActionResult<Person> Post(Person person)
+        {
+            var newPerson = _personRepository.Create(person);
+            return new JsonResult(newPerson);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public ActionResult<Person> Put(int id, [FromBody] Person person)
+        {
+            Person updatedPerson = _personRepository.Update(id, person);
+            return new JsonResult(updatedPerson);
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            _personRepository.Delete(id);
+            return new OkResult();
+        }
+    }
+}
---
+++ WebApiTask1/WebApiTask1/Repositories/PersonRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using WebApiTask1.Models;
+
+namespace WebApiTask1.Repositories
+{
+    public class PersonRepository : IPersonRepository
+    {
+
+        private readonly PersondbContext _context;
+
+        public PersonRepository(PersondbContext context)
+        {
+            _context = context;
+
+        }
+
+
+        public Person Create(Person person)
+        {
+            _context.Add(person);
+            _context.SaveChanges();
+            return person;
+        }
+
+        public List<Person> Read()
+        {
+            return _context.Person
+                .Include(p=>p.Phone)
+                .ToList();
+        }
+
+        public Person Read(int id)
+        {
+            return _context.Person
+                .Include(p=>p.Phone)
+                .FirstOrDefault(p => p.Id == id);
+        }
+
+        public Person Update(int id, Person person)
+        {
+            _context.Update(person);
+            _context.SaveChanges();
+            return person;
+        }
+
+        public void Delete(int id)
+        {
+            var person = Read(id);
+            _context.Person.Remove(person);
+            _context.SaveChanges();
+            return;
+        }
+    }
+}
